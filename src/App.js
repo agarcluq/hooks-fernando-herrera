4999267eed3c8903da9a0a120d7cbe623db45f70
agarcluq <agarcluq@everis.com>
2021-05-03 12:58:27 +0200

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./style.css";
-import CounterWithCustomHook from "./components/01-useState/CounterWithCustomHook";
-
+// import CounterWithCustomHook from "./components/01-useState/CounterWithCustomHook";
+import SimpleForm from "./components/02-useEffect/SimpleForm"
 export default function App() {
 
   return (
     <div>
       {/* <CounterApp/> */}
-      <CounterWithCustomHook/>
+      {/* <CounterWithCustomHook/> */}
+      <SimpleForm/>
     </div>  
   );
 }
---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+import useCounter from "../../hooks/useCounter"
+
+const SimpleForm = () => {
+
+  return (
+    <>
+      <h1>Use effect</h1>
+      <hr/>
+
+    </>  
+  )
+}
+export default SimpleForm;
