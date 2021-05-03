3428be86175acb85d1b01791517c83ab1fb952a7
agarcluq <agarcluq@everis.com>
2021-05-03 11:31:55 +0200

    1 file modified

--- src/components/01-useState/CounterApp.js
+++ src/components/01-useState/CounterApp.js
@@ -5,15 +5,18 @@ import React,{useState} from "react";
 export default function App() {
 const [state,setState] =useState({
   counter1:10,
-  counter2:20
+  counter2:20,
+  counter3:30,
+  counter4:20
 });
 
-const {counter1,counter2}=state;
+const {counter1,counter2,counter4}=state;
 
   return (
     <>
       <p>Contador 1: {counter1}</p>
       <p>Contador 2: {counter2}</p>
+      <p>Contador 4: {counter4}</p>
       <button onClick= {
         () => {
         setState({
