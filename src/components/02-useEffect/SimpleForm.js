40363429da9ffae95fa2ee5f19173df5a9147286
agarcluq <agarcluq@everis.com>
2021-05-03 13:09:04 +0200

    1 file modified

--- src/components/02-useEffect/SimpleForm.js
+++ src/components/02-useEffect/SimpleForm.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import useCounter from "../../hooks/useCounter"
 
 const SimpleForm = () => {
-
+  const [formState,setFormState] = useState({
+    name: '',
+    email: ''
+  })
+  const {name,email} = formState;
+useEffect ( () =>{
+  console.log('hola')
+})
+const handleInputChange = ({target}) => {
+  console.log(e.target.name)
+  console.log(e.target.value)
+  setFormState({
+    ...formState,
+    [target.name]: target.value
+    
+  })
+}
   return (
     <>
       <h1>Use effect</h1>
       <hr/>
+      <form>
+      <input
+      type="text"
+      name="name"
+      placeholder="tu nombre"
+      autoComplete="off"
+      value={name}
+      onChange = {handleInputChange}
+
+      />
+      </form>
 
     </>  
   )
