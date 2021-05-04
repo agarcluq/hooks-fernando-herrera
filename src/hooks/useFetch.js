f29f38e674f4aba6ef8e99c7a47d57dc0e26f960
agarcluq <agarcluq@everis.com>
2021-05-04 09:28:18 +0200

    1 file modified

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,9 +1,38 @@
-import {useState} from 'react'
-
-const useFetch = (initialState = {}) =>{
-const [values,setValues] = useState(initialState)
+import { useState, useEffect, useRef } from 'react';
 
 
-  return []
+export const useFetch = ( url ) => {
+    
+    const isMounted = useRef(true);
+    const [state, setState] = useState(
+      { data: null, loading: true, error: null });
+
+    useEffect( () => {
+        return () => {
+            isMounted.current = false;
+        }
+    }, [])
+
+
+    useEffect( () => {
+
+        setState({ data: null, loading: true, error: null });
+
+        fetch( url )
+            .then( resp => resp.json() )
+            .then( data => {
+
+                if ( isMounted.current ) {
+                    setState({
+                        loading: false,
+                        error: null,
+                        data
+                    });
+                }
+
+            });
+
+    },[url])
+
+    return state;
 }
-export default useFetch;
