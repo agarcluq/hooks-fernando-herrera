8260ec223c0191a32fa90156a8dc2b29ac79d157
agarcluq <agarcluq@everis.com>
2021-05-04 09:51:34 +0200

    1 file modified

--- src/components/03-examples/MultipleCustomHooks.js
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -1,10 +1,13 @@
 import React,{useEffect,useState} from "react";
 import useFetch from "../../hooks/useFetch";
+import useCounter from "../../hooks/useCounter";
 
 const MultipleCustomHooks = () => {
-const {loading,data} = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
+const {counter,increment} = useCounter(1)
+const {loading,data} = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
 const {author,quote} = !!data && data[0] //si existe la data sacame la posición 0 de la data
 console.log(author,quote)
+
   return (
     <>
       <h1>CreakingBad Quotes</h1>
@@ -18,7 +21,7 @@ console.log(author,quote)
               </div>
         )
       }
-      <button>Next</button>
+      <button onClick={()=>setCounter(counter+1)}>Next</button>
      
     </>  
   )
