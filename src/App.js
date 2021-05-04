b55c37ff2e1b7170864353e374c1ec973de6c6fa
agarcluq <agarcluq@everis.com>
2021-05-04 10:10:54 +0200

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import "./style.css";
 // import CounterWithCustomHook from "./components/01-useState/CounterWithCustomHook";
 // import FormWithCustomHooks from "./components/02-useEffect/FormWithCustomHooks"
 
-import MultipleCustomHooks from "./components/03-examples/MultipleCustomHooks"
-export default function App() {
+// import MultipleCustomHooks from "./components/03-examples/MultipleCustomHooks"
 
+import FocusScreen from "./components/04-useRef/FocusScreen"
+
+export default function App() {
   return (
     <div>
       {/* <CounterApp/> */}
       {/* <CounterWithCustomHook/> */}
       {/* <SimpleForm/> */}
       {/* <FormWithCustomHooks/> */}
-      <MultipleCustomHooks/>
+      {/* <MultipleCustomHooks/> */}
+      <FocusScreen/>
     </div>  
   );
 }
