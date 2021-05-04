55fc461ee1a5bf8a75af3ef041abf6768afb552e
agarcluq <agarcluq@everis.com>
2021-05-04 09:47:18 +0200

    1 file modified, 1 added

--- src/components/03-examples/MultipleCustomHooks.js
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -2,23 +2,24 @@ import React,{useEffect,useState} from "react";
 import useFetch from "../../hooks/useFetch";
 
 const MultipleCustomHooks = () => {
-const {loading} = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
-console.log(loading)
+const {loading,data} = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
+const {author,quote} = !!data && data[0] //si existe la data sacame la posición 0 de la data
+console.log(author,quote)
   return (
     <>
       <h1>CreakingBad Quotes</h1>
-  {
-    loading 
-    ? (<div>Loading ...</div>)
-    :(
-          <div>
-            <p>Hola mundo</p>
-            <small>- Fernando</small>
-          </div>
-    )
-  }
+      {
+        loading 
+        ? (<div>Loading ...</div>)
+        :(
+              <div>
+                <p>{quote}</p>
+                <small>- {author}</small>
+              </div>
+        )
+      }
+      <button>Next</button>
      
-
     </>  
   )
 }
